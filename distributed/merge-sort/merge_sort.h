18ba39a01986da13a03222eb0d9c39b01eaa354e
916-Andrei-Saramet <andreisaramet21@gmail.com>
2023-01-20 14:07:48 +0200

    merge-sort implementation

---
+++ distributed/merge-sort/merge_sort.h
@@ -0,0 +1,84 @@
+#ifndef DISTRIBUTED_MERGE_SORT_H
+#define DISTRIBUTED_MERGE_SORT_H
+
+#include <vector>
+
+void concat(const std::vector<int> &v1,
+            const std::vector<int> &v2,
+            std::vector<int> &res);
+
+void halve(const std::vector<int> &source,
+           std::vector<int> &firstHalf,
+           std::vector<int> &secondHalf);
+
+void merge_rec(const std::vector<int>::const_iterator &v1_begin,
+               const std::vector<int>::const_iterator &v1_end,
+               const std::vector<int>::const_iterator &v2_begin,
+               const std::vector<int>::const_iterator &v2_end,
+               std::vector<int> &result,
+               const std::function<bool(const int &, const int &)> &comparator = [](const int &a,
+                                                                                    const int &b) -> bool {
+                   return a < b;
+               });
+
+void merge_rec(const int &threadsNo,
+               const std::vector<int>::const_iterator &v1_begin,
+               const std::vector<int>::const_iterator &v1_end,
+               const std::vector<int>::const_iterator &v2_begin,
+               const std::vector<int>::const_iterator &v2_end,
+               std::vector<int> &result,
+               const std::function<bool(const int &, const int &)> &comparator = [](const int &a,
+                                                                                    const int &b) -> bool {
+                   return a < b;
+               });
+
+void merge(const std::vector<int>::const_iterator &v1_begin,
+           const std::vector<int>::const_iterator &v1_end,
+           const std::vector<int>::const_iterator &v2_begin,
+           const std::vector<int>::const_iterator &v2_end,
+           std::vector<int> &result,
+           const std::function<bool(const int &, const int &)> &comparator = [](const int &a,
+                                                                                const int &b) -> bool {
+               return a < b;
+           });
+
+void merge_rec(const std::vector<int> &v1,
+               const std::vector<int> &v2,
+               std::vector<int> &result,
+               const std::function<bool(const int &, const int &)> &comparator = [](const int &a,
+                                                                                    const int &b) -> bool {
+                   return a < b;
+               });
+
+void merge_rec(const int &threadsNo,
+               const std::vector<int> &v1,
+               const std::vector<int> &v2,
+               std::vector<int> &result,
+               const std::function<bool(const int &, const int &)> &comparator = [](const int &a,
+                                                                                    const int &b) -> bool {
+                   return a < b;
+               });
+
+void merge(const std::vector<int> &v1,
+           const std::vector<int> &v2,
+           std::vector<int> &result,
+           const std::function<bool(const int &, const int &)> &comparator = [](const int &a, const int &b) -> bool {
+               return a < b;
+           });
+
+
+void merge_sort(std::vector<int> &v,
+                const std::function<bool(const int &, const int &)> &comparator = [](const int &a,
+                                                                                     const int &b) -> bool {
+                    return a < b;
+                });
+
+void merge_sort(const int &threadsNo,
+                std::vector<int> &v,
+                const std::function<bool(const int &, const int &)> &comparator = [](const int &a,
+                                                                                     const int &b) -> bool {
+                    return a < b;
+                });
+
+
+#endif //DISTRIBUTED_MERGE_SORT_H
